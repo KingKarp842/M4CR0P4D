615683a68b61e6719f7d6ac14cacbd16e295546e
Preston Nguyen <prestonjhnguyen@gmail.com>
2026-06-20 19:22:03 -0700

    Update main.py

--- KMK Firmware/main.py
+++ KMK Firmware/main.py
@@ -3,54 +3,34 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
-from kmk.modules.layers import Layers
-from kmk.extensions.media_keys import MediaKeys
 
 keyboard = KMKKeyboard()
 
-layers = Layers()
-keyboard.modules.append(layers)
-
-media = MediaKeys()
-keyboard.extensions.append(media)
-
 keyboard.matrix = KeysScanner(
     pins=[
-        board.D1,   # SW1
-        board.D2,   # SW2
-        board.D3,   # SW3
-        board.D4,   # SW4
-        board.D7,   # SW5
-        board.D8,   # SW6
-        board.D9,   # SW7
-        board.D10,  # SW8
+        board.D0,
+        board.D1,
+        board.D2,
+        board.D3,
+        board.D6,
+        board.D7,
+        board.D8,
+        board.D9,
     ],
     value_when_pressed=False,
     pull=True,
 )
 
-keyboard.keymap = [
-    [  # Layer 0
-        KC.COPY,
-        KC.PASTE,
-        KC.CUT,
-        KC.UNDO,
-        KC.REDO,
-        KC.MUTE,
-        KC.VOLU,
-        KC.MO(1),
-    ],
-    [  # Layer 1
-        KC.F1,
-        KC.F2,
-        KC.F3,
-        KC.F4,
-        KC.F5,
-        KC.F6,
-        KC.F7,
-        KC.TRNS,
-    ],
-]
+keyboard.keymap = [[
+    KC.LGUI(KC.C),
+    KC.LGUI(KC.V),
+    KC.F1,
+    KC.LGUI(KC.L),
+    KC.BSPC,
+    KC.LGUI(KC.A),
+    KC.LGUI(KC.T),
+    KC.LGUI(KC.W),
+]]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     keyboard.go()
